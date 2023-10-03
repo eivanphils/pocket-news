0bf575dd6c6f568b1ef54abfe2ca0ae7065b6755
Phils Garcia <eivanphils@gmail.com>
2023-10-03 18:09:12 -0300

    Modificado el modelo y

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,3 +1,3 @@
 export 'package:pocket_news/models/article_model.dart';
-export 'package:pocket_news/models/news_models.dart';
+export 'package:pocket_news/models/news_model.dart';
 export 'package:pocket_news/models/source_model.dart';
